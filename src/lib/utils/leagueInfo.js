081c252880d368d11c285abef61ffb8e8f8c5799
bpeterson8412 <bpeterson8412@gmail.com>
2025-08-30 02:54:14 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -46,6 +46,11 @@ export const managers = [
          link: 1, // manager array number within this array, or null to link back to all managers page
          image: "/managers/BN.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
          },
+     //  "rival2": {
+     //    name: "BN", // Can be anything (usually your rival's name)
+     //    link: 1, // manager array number within this array, or null to link back to all managers page
+     //    image: "/managers/BN.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+     //    },
     //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
        "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
@@ -67,6 +72,11 @@ export const managers = [
          name: "BP", // Can be anything (usually your rival's name)
          link: 0, // manager array number within this array, or null to link back to all managers page
          image: "/managers/bpdodge.JPG", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+         },
+        "rival": {
+         name: "MacDaddy", // Can be anything (usually your rival's name)
+         link: 2, // manager array number within this array, or null to link back to all managers page
+         image: "/managers/lilpiggy.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
          },
     //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
