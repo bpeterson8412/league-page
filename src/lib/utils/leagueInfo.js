f672d85bdcb721ca13770ff6521b322f14e3b626
bpeterson8412 <bpeterson8412@gmail.com>
2025-08-30 02:46:29 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -63,16 +63,11 @@ export const managers = [
        "fantasyStart": "The Beginning", // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-       "rival1": {
+       "rival": {
          name: "BP", // Can be anything (usually your rival's name)
          link: 0, // manager array number within this array, or null to link back to all managers page
          image: "/managers/bpdodge.JPG", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
          },
-       "rival2": {
-         name: "MacDaddy",
-         link: 2,
-         image: "/managers/lilpiggy.png",
-       },
     //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
        "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
