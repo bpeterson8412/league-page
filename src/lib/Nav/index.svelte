8e357ea685f90376fc69bb2cc1e4be303b581600
bpeterson8412 <bpeterson8412@gmail.com>
2025-09-06 14:57:52 -0500

    Update index.svelte

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -76,7 +76,7 @@
 </style>
 
 <nav>
-	<a href="/"><img id="logo" alt="league logo" src="/badge.png" /></a>
+	<a href="/"><img id="logo" alt="league logo" src="/MUFFCupYr.png" /></a>
 
 	<div class="container">
 		<IconButton
